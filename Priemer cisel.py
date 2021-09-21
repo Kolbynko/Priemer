82905bd4299c313e026722c8ee62202c771f85ef
Kolbynko <90548034+Kolbynko@users.noreply.github.com>
2021-09-21 09:08:36 +0200

    Priemer n čísel

--- Priemer cisel.py
+++ Priemer cisel.py
@@ -1,5 +1,15 @@
-a=int(input('Zadaj prvé číslo: '))
-b=int(input('Zadaj druhé číslo: '))
-c=int(input('Zadaj tretie číslo: '))
-
-print('Priemer čísel je', int((a+b+c)/3))
+#average = x
+#pocet cisel = z
+z=0
+x=0
+pokracuj='y'
+while pokracuj == 'y':
+  a=int(input('Zadaj číslo: '))
+  pokracuj=str(input('Chceš pokračovať? y/n: '))
+  else:
+    z=z+1
+    x=x+a
+  if pokracuj == 'n':
+    print('Priemer čísel je', x/z)
+  
+  
